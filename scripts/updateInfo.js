2cf8253af7cb98580cb63724a9b68706aee05bdb
Sandeep Kumar <sandeepkumar@Sandeeps-MacBook-Air.local>
2021-10-21 07:47:18 +0530

    file gen

--- scripts/updateInfo.js
+++ scripts/updateInfo.js
@@ -3,25 +3,29 @@ const cloudformation = new AWS.CloudFormation();
 const parameters = require("../parameters.json");
 const fs = require("fs");
 
-const response = await cloudformation
-  .describeStacks({
+cloudformation.describeStacks(
+  {
     StackName: parameters.StackName,
-  })
-  .promise();
+  },
+  function (err, data) {
+    if (err) console.log(err, err.stack);
+    else {
+      console.log(data);
+      let output = {};
 
-let output = {};
-
-response.Stacks[0].Parameters.forEach((param) => {
-  output[param.ParameterKey] = param.ParameterValue;
-});
-response.Stacks[0].Outputs.forEach((param) => {
-  output[param.OutputKey] = param.OutputValue;
-
-  fs.writeFile(
-    "../s3-website/src/leaveApp/config2.json",
-    JSON.stringify(output, null, 4),
-    (err) => {
-      if (err) console.log(err);
+      response.Stacks[0].Parameters.forEach((param) => {
+        output[param.ParameterKey] = param.ParameterValue;
+      });
+      response.Stacks[0].Outputs.forEach((param) => {
+        output[param.OutputKey] = param.OutputValue;
+      });
+      fs.writeFile(
+        "../s3-website/src/leaveApp/config2.json",
+        JSON.stringify(output, null, 4),
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
     }
-  );
-});
+  }
+);
