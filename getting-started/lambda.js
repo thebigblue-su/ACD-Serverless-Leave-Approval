2eda6392efd0ac6ee357423e66132a846a6f490e
Sandeep Kumar <sandeepkumar@Sandeeps-MacBook-Air.local>
2021-10-22 22:37:42 +0530

    Updated readme.md

--- getting-started/lambda.js
+++ getting-started/lambda.js
@@ -18,13 +18,19 @@ const getUserInfo = async (userInfo) => {
 exports.handler = async (event, context) => {
   console.log(event);
   // Local Testing
-  // let email = event.email
+  let email = event.email;
 
   // Lambda Proxy Testing
-  let email = event.queryStringParameters.email;
+  // let email = event.queryStringParameters.email;
 
+  // API return statement
   return {
     statusCode: 200,
     body: JSON.stringify(await getUserInfo(email)),
   };
+
+  // Step Function return statement
+  //   return {
+  //     foo: 2,
+  //   };
 };
